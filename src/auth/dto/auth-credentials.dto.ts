3e7172c7e4c990b380df3ccc02b3047970809b9d
thinhnnd <16521179@gm.uit.edu.vn>
2020-09-01 09:12:32 +0700

    feat: validate username & password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,18 @@
+import { } from 'class-transformer';
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        {message: 'Password to week. Your password must contain number special character and an uppercase'}
+        )
     password: string;
 }
