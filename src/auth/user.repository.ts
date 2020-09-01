994e00fce5e032d95a10afb29208729ee826ede5
thinhnnd <16521179@gm.uit.edu.vn>
2020-09-01 10:41:57 +0700

    feat: simple signin function

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,5 @@
 import { Repository, EntityRepository } from "typeorm";
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { ConflictException, InternalServerErrorException, UnauthorizedException } from "@nestjs/common";
 import * as bcrypt from 'bcrypt'
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -23,9 +23,19 @@ export class UserRepository extends Repository<User> {
             } else {
                 throw new InternalServerErrorException();
             }
-
         }
+    }
 
+    async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        
+        const user = await this.findOne({ username });
+
+        if( user && await user.validatePassword(password)) {
+            return user.username;
+        } else {
+            return null;
+        }
     }
 
     private async hashPassword(password: string, salt: string): Promise<string> {
