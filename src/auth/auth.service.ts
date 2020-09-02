563bfb613cd349153cfe55102f215fd0c6257c60
thinhnnd <16521179@gm.uit.edu.vn>
2020-09-02 21:17:47 +0700

    feat: add simple logger for app

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -7,6 +7,8 @@ import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+    private logger = new Logger(`AuthService`);
+
     constructor(
         @InjectRepository(UserRepository) 
         private userRepository: UserRepository,
@@ -25,7 +27,8 @@ export class AuthService {
 
         const payload: JwtPayload = { username };
         const accessToken = this.jwtService.sign(payload);
-        
+        this.logger.debug(`Generated JWT token with payload ${JSON.stringify(payload)}`);
+
         return { accessToken };
     }
 }
--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,9 +4,13 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { User } from '../auth/user.entity';
+import { Logger, InternalServerErrorException } from "@nestjs/common";
+import { create } from "domain";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+
+    private logger = new Logger('TaskRepository');
 
     async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
         const {status, search} = filterDto;
@@ -22,9 +26,13 @@ export class TaskRepository extends Repository<Task> {
             query.andWhere('(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search) )', { search: `%${search}%` });
         }
 
-        const tasks = await query.getMany();
-
-        return tasks;
+        try {
+            const tasks = await query.getMany();
+            return tasks;
+        } catch (error) {
+            this.logger.error(`Failed to get tasks for user "${user.username}", DTO: ${JSON.stringify(filterDto)}`, error.stack);
+            throw new InternalServerErrorException();
+        }
     }
 
     async createTask(
@@ -37,12 +45,16 @@ export class TaskRepository extends Repository<Task> {
         task.title = title;
         task.description = description;
         task.status = TaskStatus.OPEN;
-
         task.user = user;
-        await task.save();
+        
+        try {
+            await task.save();
+        } catch (error) {
+            this.logger.error(`Failed to create a task for user "${user.username}". Data: ${createTaskDto}`, error.stack);
+            throw new InternalServerErrorException();
+        }
 
         delete task.user;
-
         return task;
     }
 }
