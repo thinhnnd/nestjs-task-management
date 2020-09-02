7dc24f444db51dab4cbe73b59d4f8dc7dd9c0057
thinhnnd <16521179@gm.uit.edu.vn>
2020-09-02 22:53:41 +0700

    fix: some error with config

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule); 
+
+  if(process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const serverConfig = config.get('server');
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
